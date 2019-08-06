30f6279bed6fe442a3dcca1a951ff43fe7e8551c
chrismjohnston <cmjohnston@gmail.com>
2019-08-06 17:01:36 -0500

    eight down two to go

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 
-//mouse over nav items and they change color to red
+// 1 mouse over nav items and they change color to red
 const mouseOver = document.querySelector('nav');
 
 mouseOver.addEventListener(
@@ -13,15 +13,15 @@ mouseOver.addEventListener(
     false
   );
 
- //keypress make the entire page disappear
+ //2 keypress make the entire page disappear
 const gone = document.querySelector("body");
 gone.addEventListener("keydown", () => (gone.style.opacity = "0"));
 
-//moving mouse makes text come back
+//3 moving mouse makes text come back
 const back = document.querySelector("body");
 back.addEventListener("mouseenter", () => (back.style.opacity = "1"));
 
-//double click on signup button and you get an alert have your credit card number ready
+//4 double click on signup button and you get an alert have your credit card number ready
 const signupButton = document.querySelectorAll(".btn");
 
 signupButton.forEach(node => {
@@ -30,19 +30,19 @@ signupButton.forEach(node => {
   });
 });
 
-//on event load it says your page is loaded and comes to the unscientific conclusion that Google thinks it is fast.
+//5 on event load it says your page is loaded and comes to the unscientific conclusion that Google thinks it is fast.
 window.addEventListener("load", function() {
     alert("Your page has loaded and Google says your page speed is fast");
   });
 
-  //drag the bus photo and it gets bigger by 125%
+//6 drag the bus photo and it gets bigger by 125%
 const busPhoto = document.querySelector(".intro img");
 busPhoto.addEventListener(
   "drag",
   e => (e.target.style.transform = "scale(1.25)")
 );
 
-//moving mouse over destination titles turns them green
+//7 moving mouse over destination titles turns them green
 const destinationTitle = document.querySelectorAll(".destination h4");
 
 destinationTitle.forEach(node => {
@@ -58,6 +58,14 @@ destinationTitle.forEach(node => {
     ) ;
 })
 
+//8 click on first destination heading and alert wind tells you 'have fun in the sun'
+const linkClick = document.querySelector("h4");
+
+linkClick.addEventListener("click", function(event) {
+    alert("Have Fun In The Sun!");
+  });
+
+  //stop nav links from reloading page
 document.getElementsByClassName("nav-link").addEventListener("click", function(event){
     event.preventDefault();
    handleFireButton();
