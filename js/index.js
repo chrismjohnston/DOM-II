7bb9cccd4348d16c0051cc5d5c45501fc777df02
chrismjohnston <cmjohnston@gmail.com>
2019-08-06 15:50:36 -0500

    three down seven to go

--- js/index.js
+++ js/index.js
@@ -12,3 +12,11 @@ mouseOver.addEventListener(
     },
     false
   );
+
+  //keypress make the entire page disappear
+const gone = document.querySelector("body");
+gone.addEventListener("keydown", () => (gone.style.opacity = "0"));
+
+//moving mouse makes text come back
+const back = document.querySelector("body");
+back.addEventListener("mouseenter", () => (back.style.opacity = "1"));
