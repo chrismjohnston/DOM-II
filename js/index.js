b23655f49482f918cc65c2b3cf6c19924f12032f
chrismjohnston <cmjohnston@gmail.com>
2019-08-06 15:45:47 -0500

    one down nine to go

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
-// Your code goes here
+
+//mouse over nav items and they change color to red
+const mouseOver = document.querySelector('nav');
+
+mouseOver.addEventListener(
+    "mouseover",
+    function(event) {
+      event.target.style.color = "red";
+      setTimeout(function() {
+        event.target.style.color = "";
+      }, 250);
+    },
+    false
+  );
