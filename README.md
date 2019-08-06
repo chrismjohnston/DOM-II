6fa8f15c4167d0dd32c2ea562399a5ba4ce25bd8
chrismjohnston <cmjohnston@gmail.com>
2019-08-06 16:02:47 -0500

    five down five to go

--- README.md
+++ README.md
@@ -37,6 +37,7 @@ Fun Bus wants you to make their site more interactive. They are relying on you t
 	* [x] `mouseover`
 	* [x] `keydown`
 	* [x] `mousemove`
+	* [x] `mousemove`	
 	* [ ] `wheel`
 	* [ ] `drag / drop`
 	* [ ] `load`
--- js/index.js
+++ js/index.js
@@ -20,3 +20,17 @@ gone.addEventListener("keydown", () => (gone.style.opacity = "0"));
 //moving mouse makes text come back
 const back = document.querySelector("body");
 back.addEventListener("mouseenter", () => (back.style.opacity = "1"));
+
+//double click on signup button and you get an alert have your credit card number ready
+const signupButton = document.querySelectorAll(".btn");
+
+signupButton.forEach(node => {
+  node.addEventListener("dblclick", function() {
+    alert(`have your CC number ready`);
+  });
+});
+
+//on event load it says your page is loaded and comes to the unscientific conclusion that Google thinks it is fast.
+window.addEventListener("load", function() {
+    alert("Your page has loaded and Google says your page speed is fast");
+  });
